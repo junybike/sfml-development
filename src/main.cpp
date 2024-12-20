afb7cc90f6dfcd196c624c1cfc66767f0f6f37da
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-12-19 17:25:21 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+
+#include "../include/game.h"
+
+int main()
+{
+    Game game;
+
+    while (!game.GetWindow()->IsDone())
+    {
+        game.Update();
+        game.Render();
+    }
+}
+
